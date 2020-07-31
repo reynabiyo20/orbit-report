9eec9142aee0192459235faf4eb7a18f7e165e70
reyna biyo <reynabiyo20@gmail.com>
2020-07-31 13:49:52 -0500

    Defined & Created Satellites Class,Created Orbit List component, Pass in Satellites to Orbit List Component, Display tables of Satellites,Fetch Satellite dat

--- lc101/Orbit-Report/orbit-report/src/app/satellite.ts
+++ lc101/Orbit-Report/orbit-report/src/app/satellite.ts
@@ -1,18 +1,28 @@
 export class Satellite {
-name: string;
-orbitType: string;
-type: string;
-operational: boolean;
-launchDate: string;
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
 
-constructor (name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
-this.name = name;
-this.orbitType = orbitType;
-this.type = type;
-this.operational = operational;
-this.launchDate =launchDate;
-}
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
 
-}
+        this.name = name;
+        this.type = type;
+        this.operational = operational;
+        this.orbitType = orbitType;
+        this.launchDate = launchDate;
+
+    };
+
+    shouldShowWarning() {
+        if (this.type = "Space Debris") {
+            return true
+        } else {
+            return false
+        }
+    };
+
+};
 
 
